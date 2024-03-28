570267e7f04f5c5b6ae818e91b2283ec02f7d5b1
NatNichols <natalie.e.nichols@gmail.com>
2024-03-28 18:16:04 -0400

    stub up TvShowSearch component

---
+++ src/pages/TvShowSearch/TvShowSearch.jsx
@@ -0,0 +1,12 @@
+// css
+import styles from './TvShowSearch.module.css'
+
+const TvShowSearch = () => {
+  return (
+    <>
+      <h1>TV Show Search</h1>
+    </>
+  )
+}
+
+export default TvShowSearch
