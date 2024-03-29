8c2e8158cc8315f8d1441243789f66fa91ae56b0
NatNichols <natalie.e.nichols@gmail.com>
2024-03-29 12:41:07 -0400

    stub up search form and prep for service fn

--- src/pages/TvShowSearch/TvShowSearch.jsx
+++ src/pages/TvShowSearch/TvShowSearch.jsx
@@ -1,10 +1,33 @@
+// npm modules
+import { useState } from 'react'
+
 // css
 import styles from './TvShowSearch.module.css'
 
 const TvShowSearch = () => {
+  const [formData, setFormData] = useState({
+    query: ''
+  })
+  
+  const [results, setResults] = useState([])
+
+  const handleChange = evt => {
+    setFormData({...formData, [evt.target.name]: evt.target.value})
+  }
+
+  const handleSubmit = evt => {
+    evt.preventDefault()
+    // MAKE API CALL USING STATE
+    // SET RESULTS WITH RETURNED DATA
+  }
+
   return (
     <>
       <h1>TV Show Search</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="query" onChange={handleChange}/>
+        <button type="submit">Search</button>
+      </form>
     </>
   )
 }
